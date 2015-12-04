c2dc8ffb08de21a279fa5f7dd45d8eb69796f0fa
barreira89 <barreira89@gmail.com>
2015-12-04 14:57:09 -0500

    Latest

--- public/javascripts/controllers/MainController.js
+++ public/javascripts/controllers/MainController.js
@@ -1,5 +1,59 @@
 app.controller('MainController', ['$scope', function($scope){
 	var test = "First Test"
 	$scope.test = test;
-	
+	$scope.schedule =
+		{	
+			"Week" : 1,
+			"Teams" : [{
+					"visitor" : "Steelers",
+					"home" : "Patriots"
+				}, {
+					"visitor" : "Packers",
+					"home" : "Bears"
+				}, {
+					"visitor" : "Chiefs",
+					"home" : "Texans"
+				}, {
+					"visitor" : "Browns",
+					"home" : "Jets"
+				}, {
+					"visitor" : "Colts",
+					"home" : "Bills"
+				}, {
+					"visitor" : "Dolphins",
+					"home" : "Redskins"
+				}, {
+					"visitor" : "Panthers",
+					"home" : "Jaguars"
+				}, {
+					"visitor" : "Seahawks",
+					"home" : "Rams"
+				}, {
+					"visitor" : "Saints",
+					"home" : "Cardinals"
+				}, {
+					"visitor" : "Lions",
+					"home" : "Chargers"
+				}, {
+					"visitor" : "Titans",
+					"home" : "Buccaneers"
+				}, {
+					"visitor" : "Bengals",
+					"home" : "Raiders"
+				}, {
+					"visitor" : "Ravens",
+					"home" : "Broncos"
+				}, {
+					"visitor" : "Giants",
+					"home" : "Cowboys"
+				}, {
+					"visitor" : "Eagles",
+					"home" : "Falcons"
+				}, {
+					"visitor" : "Vikings",
+					"home" : "49ers"
+				}
+			]
+		}
+
 }]);
---
+++ public/javascripts/directives/footballTeams.js
@@ -0,0 +1,9 @@
+app.directive('footballTeams', function(){
+	return {
+		restrict: 'E',
+		scope: {
+			info: '='
+		},
+		templateUrl: 'public/javascripts/directives/footballTeam.html'
+		}	
+	});
