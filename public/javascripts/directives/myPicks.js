532b1e3899b73957d48677a6f6e6629f1a9ca0ad
Steve <steve@ubuntu.ubuntu-domain>
2016-08-08 17:13:04 -0400

    Updated my pick directive

--- public/javascripts/directives/myPicks.js
+++ public/javascripts/directives/myPicks.js
@@ -1,10 +1,15 @@
-app.directive('myPicks', ['users', function(users){
+app.directive('myPicks', ['users', 'picks', function(users, picks){
 	return {
 		restrict: 'E',
 		scope: {
 			userpicks: '='
 		},
 		link: function(scope, elm, attr) {
+			scope.updatePick = function (pickId, pick){
+				picks.updatePick(pickId, pick).success(function(data){
+					console.log(data);
+				});
+			}
 		},
 		templateUrl: 'public/javascripts/directives/myPicks.html'
 }}]);
